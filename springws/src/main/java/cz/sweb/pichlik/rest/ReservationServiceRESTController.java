5a55bb53347d597de2894d672a5ec8153691bfc7
Dagi <pichlik@seznam.cz>
2012-05-11 09:12:14 +0200

    fixed request mapping, reservation RES MVCT configuration simplified

--- springws/src/main/java/cz/sweb/pichlik/rest/ReservationServiceRESTController.java
+++ springws/src/main/java/cz/sweb/pichlik/rest/ReservationServiceRESTController.java
@@ -5,11 +5,13 @@
 package cz.sweb.pichlik.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import cz.sweb.pichlik.Reservation;
 import cz.sweb.pichlik.ReservationService;
@@ -21,8 +23,9 @@ public class ReservationServiceRESTController {
     @Autowired
     private ReservationService reservationService;
 
-    @RequestMapping(value="/reservation/", method=RequestMethod.POST)
+    @RequestMapping(value="/reservation", method=RequestMethod.POST)
     @ResponseBody
+    @ResponseStatus(value=HttpStatus.CREATED)
     public ReservationImpl reserveBook(@RequestBody ReservationImpl reservation) {
         Reservation reservedBook = reservationService.reserveBook(reservation.getBookId());
         return (ReservationImpl)reservedBook;
